e679cd3c5e3fdc9d9855b69c88c528dc316c9e27
Eugene Xu <eugene_xu@epam.com>
2021-05-14 11:55:19 +0800

    update chapter13[part 2]

--- src/13.sh
+++ src/13.sh
@@ -17,3 +17,24 @@ ls -l ${4:-'13.sh'} | {
 }
 
 ./onebyone.sh < 13.sh
+
+# bash有效，13.15
+# case 1
+echo 1
+while read; do echo ~~"$REPLY"~~; done < whitespace
+# case 2
+echo 2
+while read REPLY; do echo ~~"$REPLY"~~; done < whitespace
+# case 3
+echo 3
+while read; do echo "~~${REPLY## }~~"; done < whitespace
+# case 4
+echo 4
+while read; do echo "~~${REPLY%% }~~"; done < whitespace
+# case 5
+echo 5
+shopt -s extglob
+while read; do echo "~~${REPLY##+([[:space:]])}~~"; done < whitespace
+# case 6
+echo 6
+while read; do echo "~~${REPLY%%+([[:space:]])}~~"; done < whitespace
