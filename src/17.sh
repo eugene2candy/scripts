616ab6008c2ec22a645dfd30c1663204dce49ccf
Eugene Xu <eugene_xu@epam.com>
2021-05-18 16:32:02 +0800

    update 17[part 1]

---
+++ src/17.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# replace echo to mv and remove <>
+# expect files as xxx=xxx=xxx.odt
+for i in *.odt; do echo "<$i>" "<$(echo "$i" | cut -d'=' -f1,3)>"; done
+
+# unzip '*.zip'
+
+screen -a
+ls
+# screen -r
+# screen -d -r
