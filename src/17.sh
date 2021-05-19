25ffe2695f1baf6b800122779239531113d83a75
Eugene Xu <eugene_xu@epam.com>
2021-05-19 14:22:19 +0800

    update 17[part 3]

--- src/17.sh
+++ src/17.sh
@@ -18,3 +18,36 @@ sed -i '' -e '1i\
 static header line1
 ' add_header
 
+echo 仅出现在left
+comm -23 left right
+
+echo 仅出现在right
+comm -13 left right
+
+echo 同时出现
+comm -12 left right
+
+echo \
+echo diff 1
+diff -y -W 60 left right
+
+echo \
+echo diff 2
+diff -y -W 60 --suppress-common-lines left right
+
+echo \
+echo diff 3
+diff left right
+
+cat -n 17.sh
+cat -b 17.sh
+
+pause ()
+{
+    read -n 1 -p 'press any key when ready...'
+}
+
+pause
+
+LC_NUMERIC=en_US.UTF-8 printf "%'d\n" 123456789
+LC_NUMERIC=en_US.UTF-8 printf "%'f\n" 123456789.987
